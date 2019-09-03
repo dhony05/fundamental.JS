a51ef0d972b2d74aa8299ab216e53c6ba35241bb
Donelys Familia <donelysprivate@gmail.com>
2019-09-03 14:13:08 -0400

    first commit

---
+++ objects/practiceObject.js
@@ -0,0 +1,19 @@
+
+
+/********
+* Objects and properties
+with object we define key - value pairs
+* the order doesnt matter
+********/
+
+var jonh =
+{
+  fullName : 'Juan Lopez',
+  bithYear : 1995,
+  family:['Juanito','Dany','Daniela'],
+  isMarried: false,
+  job: 'programmer'
+};
+
+console.log(jonh.fullName);
+console.log(jonh.family);
